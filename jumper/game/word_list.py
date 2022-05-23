dd0f48f1e3750c37f02e40541c4c6a24154043a7
Matthew Pellét <mattpellet@byui.edu>
2022-05-23 04:06:55 -0700

    created word_list.py

---
+++ jumper/game/word_list.py
@@ -0,0 +1,13 @@
+import csv
+import random
+
+class word_list:
+    def word_list(self):
+        with open('word_list.csv') as word_list_file :
+         word_reader = csv.reader(word_list_file)
+    
+        word_choice = list(word_reader)[1:]
+        random.shuffle(word_choice)
+
+        for name in word_reader:
+            print(name)
