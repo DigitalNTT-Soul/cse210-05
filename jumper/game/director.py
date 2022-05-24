345223c0fabb6ee3ef40db1d356c00cdfe514869
DigitalNTT-Soul <dylan.ruppell@gmail.com>
2022-05-24 19:15:33 -0400

    1) fixed imports, 2) renamed "missed guesses" to "misses" and "hit guesses" to "hits", 3) fixed gameplay while-loops, 4) made sure relevant attributes get reset between rounds, 5) accommodated for current implementation of get_input method, 6) implemented Game Over messages

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,36 +1,43 @@
-from input import Input
-from output import Output
-from word_list import Word_list
+from game.user_input import User_input
+from game.output import Output
+from game.word_list import Word_list
 
 class Director:
     def __init__(self):
-        self._input = Input()
+        self._input = User_input()
         self._output = Output()
         self._word_list = Word_list()
-        self._missed_guesses = 0
-        self._hit_guesses = 0
+        self._misses = 0
+        self._hits = 0
         self._guess = ""
         self._play_new_round = True
 
     def start_game(self):
-        while True:
+        while self._play_new_round:
             self._input.set_word(self._word_list.get_random_word())
             self._output.reset_displayed_letters()
-            while self._hit_guesses < 5 and self._missed_guesses < 4:
+            self._input.reset_guessed_letters()
+            self._misses = 0
+            self._hits = 0
+
+            while self._hits < 5 and self._misses < 4:
                 # Display gamestate
-                self._output.display(self._missed_guesses)
+                self._output.display(self._misses)
 
                 # Get user's guess
-                self._guess = self._input.get_guess()
+                self._input.get_input()
+                self._guess = self._input.user_guess
+                self._input.update_lists()
                 
                 # See if user's guess was correct (function should return number of matches and list of match indices)
-                match_count, indices = self._input.validate(self._guess)
-                self._hit_guesses += match_count
+                match_count, indices = self._input.check_guess_in_word(self._guess)
+                self._hits += match_count
 
                 if match_count == 0:
-                    self._missed_guesses += 1
+                    self._misses += 1
                 else:
                     self._output.set_correct_letter(self._guess, indices)
 
             # need some sort of game over message perhaps? maybe
+            self._output.display_game_over(self._hits, self._misses)
             self._play_new_round = self._input.prompt_play_again()
