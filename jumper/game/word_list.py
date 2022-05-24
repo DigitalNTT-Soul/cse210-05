7ba3a03d3b9525893a3790372296a1d519b5ac23
Matthew Pellét <mattpellet@byui.edu>
2022-05-24 03:47:55 -0700

    added return(randomLine)

--- jumper/game/word_list.py
+++ jumper/game/word_list.py
@@ -4,4 +4,4 @@ class word_list:
     def random_words(self):
         self.lines = open('word_list.txt').read().splitlines()
         randomLine = random.choice(self.lines)
-        print(randomLine)
+        return(randomLine)
