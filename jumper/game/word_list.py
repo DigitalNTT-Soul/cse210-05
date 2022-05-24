b38fa3bc3bb7eade2971c944baf0c4969492d197
DigitalNTT-Soul <dylan.ruppell@gmail.com>
2022-05-24 16:40:15 -0400

    documented class a little bit

--- jumper/game/word_list.py
+++ jumper/game/word_list.py
@@ -1,6 +1,16 @@
 import random
 
 class Word_list:
+    """
+    Gathers and contains the list of all words used for the game, as well as 
+        offering a function for getting a random word from that list
+    
+    Public Attributes:
+        None
+    
+    Public Methods
+        get_random_word(self)       returns whole word as a string.
+    """
     def __init__(self):
         self._file = None
         self._lines = None
