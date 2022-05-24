ae3632d0785233555d60f6544fd3e050e09cdc85
Iskarr <32428946+Iskarr@users.noreply.github.com>
2022-05-24 14:10:51 -0600

    removed test functions

--- jumper/game/output.py
+++ jumper/game/output.py
@@ -1,39 +1,31 @@
 class Output:
     def __init__(self):
-        self.welcome = "Welcome to Jumper"
-        self.parachute_dude = [' ___', '/___\ ', '\   / ', ' \ / ',
+        self._welcome = "_welcome to Jumper"
+        self._parachute_dude = [' ___', '/___\ ', '\   / ', ' \ / ',
                                '  O ', ' /|\ ', ' / \ ', '      ', '^^^^^^ ']
-        self.displayed_letters = "_____"
+        self._displayed_letters = "_____"
 
     def parachute_guy(self):
-        print(self.welcome)
-        for lines in self.parachute_dude:
+        print(self._welcome)
+        for lines in self._parachute_dude:
             print(lines)
 
     def set_correct_letter(self, single_char, index):
         if index is list:
             for i in index:
-                self.displayed_letters[i] = single_char
+                self._displayed_letters[i] = single_char
         else:
-            self.displayed_letters[index] = single_char
+            self._displayed_letters[index] = single_char
 
-    def reset_displayed_letters(self):
-        self.displayed_letters = "_____"
+    def reset__displayed_letters(self):
+        self._displayed_letters = "_____"
 
     def display(self, missed_guesses):
-        print("\n" + " ".join(self.displayed_letters) + "\n")
-        for i in range(len(self.parachute_dude)):
+        print("\n" + " ".join(self._displayed_letters) + "\n")
+        for i in range(len(self._parachute_dude)):
             if i >= missed_guesses:
                 if missed_guesses == 4 and i == 4:
                     print("  X")
                 else:
-                    print(self.parachute_dude[i])
-
-
-output = Output()
-
-output.parachute_guy()
-output.display(1)
-output.display(2)
-output.display(3)
-output.display(4)
+                    print(self._parachute_dude[i])
+                    
