cb5aa0a8564edcd955ea72f274b25ce4f4de89a6
Matthew Pellét <mattpellet@byui.edu>
2022-05-24 03:46:01 -0700

    added splitlines()

--- jumper/game/word_list.py
+++ jumper/game/word_list.py
@@ -2,6 +2,6 @@ import random
 
 class word_list:
     def random_words(self):
-        self.lines = open('word_list.txt').read()
+        self.lines = open('word_list.txt').read().splitlines()
         randomLine = random.choice(self.lines)
         print(randomLine)
