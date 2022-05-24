070a3e52b5af19be0eb2012cc9d8b6d84b3bf2de
Matthew Pellét <mattpellet@byui.edu>
2022-05-24 03:44:43 -0700

    working copy of word_list.py

--- jumper/game/word_list.py
+++ jumper/game/word_list.py
@@ -1,13 +1,7 @@
-import csv
 import random
 
 class word_list:
     def random_words(self):
-        with open('word_list.csv') as word_list_file :
-         word_reader = csv.reader(word_list_file)
-    
-        word_choice = list(word_reader)[1:]
-        random.shuffle(word_choice)
-
-        for name in word_reader:
-            print(name)
+        self.lines = open('word_list.txt').read()
+        randomLine = random.choice(self.lines)
+        print(randomLine)
