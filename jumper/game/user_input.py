f5a115f25873fffe6e11c4972d30bf9e5299a6f7
ljbrys <104159889+ljbrys@users.noreply.github.com>
2022-05-24 15:51:58 -0400

    User_input class

---
+++ jumper/game/user_input.py
@@ -0,0 +1,18 @@
+class User_input:
+    """
+    An instance that  will promp user for input -verify that it is valid input and
+    store the data
+
+
+    Attributes:
+        user_guess (str)            : The guess from the user - a string length 1
+        guessed_letters (list)      : The letters that the user has guess in lowercase format
+        not_guessed_letters (list)  : The letters of the alphabet that have not been guessed yet - in lower case format
+
+
+    """
+
+    def __init__(self):
+        self.user_guess = []
+        self.guessed_letters = []
+        self.not_guessed_letters = ["a","b","c",d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
