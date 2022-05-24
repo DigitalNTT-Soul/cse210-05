291f619d4d3b01e4e1193c441c7a9fbed56a90bf
Matthew Pellét <mattpellet@byui.edu>
2022-05-24 11:17:50 -0700

    added class initializer

--- jumper/game/word_list.py
+++ jumper/game/word_list.py
@@ -1,6 +1,9 @@
 import random
 
 class word_list:
+    def __init__(self):
+        self.lines = None
+    
     def random_words(self):
         self.lines = open('word_list.txt').read().splitlines()
         randomLine = random.choice(self.lines)
