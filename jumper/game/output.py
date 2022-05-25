5a8b1c4be71cd451206f3e9addb103c719b7b683
Iskarr <32428946+Iskarr@users.noreply.github.com>
2022-05-24 19:26:27 -0600

    added comments for the output file.

--- jumper/game/output.py
+++ jumper/game/output.py
@@ -1,15 +1,21 @@
+# Class to hold all the output for the user.
 class Output:
+    # Initializes variables in the output class
+    # A welcome message, the guy with the parachute for the user to see.
+    # Lastly, a variable for the guessed letters
     def __init__(self):
         self._welcome = "Welcome to Jumper"
         self._parachute_dude = [' ___', '/___\ ', '\   / ', ' \ / ',
                                '  O ', ' /|\ ', ' / \ ', '      ', '^^^^^^ ']
         self._displayed_letters = "_____"
 
+# This method will print out the welcome message, and the parachute guy
     def parachute_guy(self):
         print(self._welcome)
         for lines in self._parachute_dude:
             print(lines)
-
+# this method will set the correct guesses by the user
+# and keep looping through the guesses replacing the underscores. 
     def set_correct_letter(self, user_guess, index):
         listified = list(self._displayed_letters)
         
@@ -17,10 +23,11 @@ class Output:
             listified[i] = user_guess
 
         self._displayed_letters = listified
-
+# This method will reset the displayed letters back to underscores
     def reset_displayed_letters(self):
         self._displayed_letters = "_____"
-
+# This method will display the underscores or correct letters and the amount of
+# parachute that is left for the user.
     def display(self, missed_guesses):
         print("\n" + " ".join(self._displayed_letters) + "\n")
         for i in range(len(self._parachute_dude)):
@@ -30,6 +37,8 @@ class Output:
                 else:
                     print(self._parachute_dude[i])
                     
+                    
+# This method will display the winning or losing state for the user. 
     def display_game_over(self, hits, misses):
         self.display(misses)
         if hits == 5 and misses < 4:
