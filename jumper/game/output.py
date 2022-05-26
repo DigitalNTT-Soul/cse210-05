af6718c81bf25ce17ef4e504d08610179cbf8aa7
DigitalNTT-Soul <dylan.ruppell@gmail.com>
2022-05-26 01:56:57 -0400

    moved, and reformatted existing documentation comments, purely for consistency's sake.

--- jumper/game/output.py
+++ jumper/game/output.py
@@ -1,34 +1,52 @@
 # Class to hold all the output for the user.
 class Output:
-    # Initializes variables in the output class
-    # A welcome message, the guy with the parachute for the user to see.
-    # Lastly, a variable for the guessed letters
+    
     def __init__(self):
+        """
+        Initializes variables in the output class
+        A welcome message, the guy with the parachute for the user to see.
+        Lastly, a variable for the guessed letters
+        """
         self._welcome = "Welcome to Jumper"
         self._parachute_dude = [' ___', '/___\ ', '\   / ', ' \ / ',
                                '  O ', ' /|\ ', ' / \ ', '      ', '^^^^^^ ']
         self._displayed_letters = "_____"
 
-# This method will print out the welcome message, and the parachute guy
+    
     def parachute_guy(self):
+        """
+        This method will print out the welcome message, and the parachute guy
+        """
         print(self._welcome)
         for lines in self._parachute_dude:
             print(lines)
-# this method will set the correct guesses by the user
-# and keep looping through the guesses replacing the underscores. 
+
+     
     def set_correct_letter(self, user_guess, index):
+        """
+        this method will set the correct guesses by the user
+        and keep looping through the guesses replacing the underscores.
+        """
         listified = list(self._displayed_letters)
         
         for i in index:
             listified[i] = user_guess
 
         self._displayed_letters = listified
-# This method will reset the displayed letters back to underscores
+
     def reset_displayed_letters(self):
+        """
+        This method will reset the displayed letters back to underscores
+        """
+        
         self._displayed_letters = "_____"
-# This method will display the underscores or correct letters and the amount of
-# parachute that is left for the user.
+
+    
     def display(self, missed_guesses):
+        """
+        This method will display the underscores or correct letters and the amount of
+        parachute that is left for the user.
+        """
         print("\n" + " ".join(self._displayed_letters) + "\n")
         for i in range(len(self._parachute_dude)):
             if i >= missed_guesses:
@@ -36,10 +54,11 @@ class Output:
                     print("  X")
                 else:
                     print(self._parachute_dude[i])
-                    
-                    
-# This method will display the winning or losing state for the user. 
+
     def display_game_over(self, hits, misses):
+        """
+        This method will display the winning or losing state for the user.
+        """
         self.display(misses)
         if hits == 5 and misses < 4:
             print("You Won! Congratulations!")
