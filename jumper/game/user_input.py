31b9a8ea76516bb8059d15562e27d48e92f99ed4
DigitalNTT-Soul <dylan.ruppell@gmail.com>
2022-05-26 01:38:07 -0400

    Further documentation of user_input.py

--- jumper/game/user_input.py
+++ jumper/game/user_input.py
@@ -7,20 +7,27 @@ class User_input:
     Attributes:
         user_guess (str)            : The guess from the user - a string length 1
         guessed_letters (list)      : The letters that the user has guess in lowercase format
-        not_guessed_letters (list)  : The letters of the alphabet that have not been guessed yet - in lower case format
         _word (string)              : Five letter word passed in from director
 
     Methods:
         get_input (self) :          Get the guessed letter from the user, makes sure it is a valid guess then stores it in guessed letter
-        update_lists (self) :       Updates the guessed_letters list, the not_guessed_letters, and updates gueseed letter to an empty string   
+        update_lists (self) :       Updates and sorts the guessed_letters list, and clears guessed letter back to an empty string   
     """
 
     def __init__(self):
+        """
+        Constructor to declare attributes
+        """
         self.user_guess = ""
         self.guessed_letters = []
         self._word = ""
 
     def get_input(self):
+        """
+        Prompt the user to enter a letter
+        Don't let the user guess the same letter multiple times
+        Saves guessed letter to public attribute for other sources to pull from.
+        """
         guess_continue = True
 
 
@@ -43,11 +50,20 @@ class User_input:
 
 
     def update_lists(self):
+        """
+        updates and sorts the list of guessed letters
+        clears user_guess back to an empty string
+        """
         self.guessed_letters.append(self.user_guess)
         self.guessed_letters.sort()
         self.user_guess = ""
 
     def check_guess_in_word(self, letter):
+        """
+        Determines the number of times the provided letter occurs in the solution word
+        Locates the indices of those matches
+        Returns number of matches and indices of matches
+        """
         match_count = self._word.count(letter)
         indices = []
         for i in range(len(self._word)):
@@ -57,9 +73,15 @@ class User_input:
         return match_count, indices
 
     def set_word(self,word):
+        """
+        Setter to change what thes olution word itself is.
+        """
         self._word = word
 
     def prompt_play_again(self):
+        """
+        prompts the user whether they would like to play again, returning True or False
+        """
         while True:
             user_response = input(f"would you like to play the game again? (y or n): ")
             if user_response.lower() == "y":
@@ -70,4 +92,7 @@ class User_input:
                 print("I am sorry that was not a valid response please respond y or n")
 
     def reset_guessed_letters(self):
+        """
+        Resets the lsit of guessed letters, to prepare for new rounds
+        """
         self.guessed_letters = []
